71a72826ad9b3832976d3ea39a912972601baeb4
Ghastly2001 <divyanshverma18may@gmail.com>
2024-08-31 17:47:53 +0530

    mutable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     // Variables
-    let num: u8 = 20;
-    println!(" Num is : {}", num);
+    let mut num: u16 = 20; //immutable variable to make it mutable use mut keyword
+    println!(" First Num is : {}", num);
+    num = 200; //error: cannot assign twice to immutable variable
+    println!(" Second Num is : {}", num);
 }
 
 //snake_case: all lowercase with underscores between words
