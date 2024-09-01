3057a2947eb4605e53f125be040187098cd25279
Ghastly2001 <divyanshverma18may@gmail.com>
2024-09-01 23:26:42 +0530

    tuples

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 fn main() {
-    // Variables
+    // 1. Variables
     let mut num: u16 = 20; //immutable variable to make it mutable use mut keyword
     println!(" First Num is : {}", num);
     num = 200; //error: cannot assign twice to immutable variable
     println!(" Second Num is : {}", num);
 
-    //Strings are of two types
-    //1. String literals -> By default string literals are of type &str
+    //2. Strings are of two types
+    //1. String literals -> By default string literals are of type &str, string literals are immutable and fixed size stored in stack
 
     let string_litreal: &str = "Divyansh";
     println!("This is String Litreal : {}", string_litreal);
 
-    //2. String -> String is a growable, mutable, owned, UTF-8 encoded string type
+    //2. String -> String is a growable, mutable, owned, UTF-8 encoded string type, String is heap allocated
     let mut new_string: String = String::from("Divyansh is coding in Rust");
     new_string.push_str(" and learning it");
     println!("This is String: {}", new_string);
+
+    //3. Tuples
+    let hero: (&str, &str, u8) = ("Spiderman", "Peter Parker", 24);
+    let (hero_name, real_name, hero_age) = hero;
+    println!(
+        "{} becomes {} at the age of {}",
+        real_name, hero_name, hero_age
+    );
 }
 
 //snake_case: all lowercase with underscores between words
