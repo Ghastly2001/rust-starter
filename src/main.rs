22f9b9a19d930b4226be4b7449e9b5dd1217d8bf
Ghastly2001 <divyanshverma18may@gmail.com>
2024-09-01 23:11:56 +0530

    string

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::string;
+
 fn main() {
     // Variables
     let mut num: u16 = 20; //immutable variable to make it mutable use mut keyword
     println!(" First Num is : {}", num);
     num = 200; //error: cannot assign twice to immutable variable
     println!(" Second Num is : {}", num);
+
+    //Strings are of two types
+    //1. String literals
+
+    let string_litreal: &str = "Divyansh";
+    println!("This is String Litreal : {}", string_litreal);
 }
 
 //snake_case: all lowercase with underscores between words
