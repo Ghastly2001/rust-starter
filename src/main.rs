92a68872d50810bbca624e47022590d555899911
Ghastly2001 <divyanshverma18may@gmail.com>
2024-09-05 23:46:37 +0530

    txt

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ fn main() {
 }
 
 fn process_mem(mem: u8) {
-    println!("The value of mem is: {}", mem);
+    println!("The value of mem is-> {}", mem);
 }
 
 fn average(num1: u8, num2: u8) -> u8 {
