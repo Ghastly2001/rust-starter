a05c49428f768f442ecc301adacef4591713defa
Ghastly2001 <divyanshverma18may@gmail.com>
2024-09-22 23:52:22 +0530

    inp1

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,14 @@ fn main() {
     let (hero_name, real_name, hero_age) = hero;
     println!(
         "{real_name} becomes {} at the age of {}",
-         hero_name, hero_age
+        hero_name, hero_age
     );
 
     //4. Functions
     let num1: u8 = 10;
     let num2: u8 = 20;
     let avg: u8 = average(num1, num2);
-    println!("The average of {} and {} is {}", num1, num2, avg);
+    println!("The average of {num1} and {} is {}", num2, avg);
 
     let name: &str = "Ghastly";
     let uppercase_name: String = uppercase_name(name);
